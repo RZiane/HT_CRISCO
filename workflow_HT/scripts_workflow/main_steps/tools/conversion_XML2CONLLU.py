9e7a367e908ba576688d079602b7fdd65aa018af
RZiane <77019730+RZiane@users.noreply.github.com>
2023-03-01 14:41:22 +0100

    Delete workflow_HT directory

--- workflow_HT/scripts_workflow/main_steps/tools/conversion_XML2CONLLU.py
+++
@@ -1,7 +0,0 @@
-import sys, getopt
-from tools.utils import def_args
-from tools.utils import conversion_xml2conllu
-                
-if __name__ == "__main__":
-    inputfile, outputfile = def_args(sys.argv[1:])
-    conversion_xml2conllu(inputfile, outputfile)
