5f753a59a161f8aef357246a7dad53ab21d38b73
RZiane <77019730+RZiane@users.noreply.github.com>
2023-05-15 13:16:58 +0100

    Update lemmatisation.py

--- workflow_HT/scripts_workflow/main_steps/tools/lemmatisation.py
+++ workflow_HT/scripts_workflow/main_steps/tools/lemmatisation.py
@@ -39,13 +39,10 @@ def process_lemmatisation(inputfile, outputfile, d_CorrTable, d_PRESTO):
     for w in tqdm(root.findall('.//w')):
 
         # lecture des tokens
-        s_token = w.text
         if len(list(w))!=0:
             if list(w)[0].tag == 'choice':
                 s_token = list(w)[0][1].text
-
             else:
-
                 for child in w.iter():
                     if child.text:
                         s_token += child.text
